cac24952a40dd1c4398a5b75a1e2174d317d9988
Vijeth Shetti <108718187+Vijeth24@users.noreply.github.com>
2025-11-10 13:20:10 +0530

    updated js file

--- dogBio.js
+++ dogBio.js
@@ -1,12 +1,25 @@
+// Day 7 Challenge :-
 const myName = "Dev Rockey";
 const luckyNumber = 1;
 
-let nextYearLuckyNumber = luckyNumber+10;
+let nextYearLuckyNumber = luckyNumber + 10;
 let remaninder = nextYearLuckyNumber % 5;
-console.log("Hello, my name is "+myName+" .");
+console.log("Hello, my name is " + myName + " .");
 console.log(`My current lucky number is ${luckyNumber}.`);
 console.log(`My current lucky number + 10 is ${nextYearLuckyNumber}.`);
-console.log("The remainder when dividing that by 5 is " + remaninder+" .");
+console.log("The remainder when dividing that by 5 is " + remaninder + " .");
 
+// Day 8 Challenge :-
+const birthYear = 2009;
+const currentYear = 2025;
+let age = currentYear - birthYear;
+if (age < 25) {
+  console.log("Still early in the career");
+} else {
+  console.log("Well into the career phase.");
+}
 
-
+const interestsCount = 5;
+for(i=0; i<interestsCount; i++) {
+    console.log("Interests number "+[i+1]+" checked!");
+}
