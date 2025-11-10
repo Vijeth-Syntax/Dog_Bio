544dc68f25cad70d9514ce97e216a944dd85583f
Vijeth Shetti <108718187+Vijeth24@users.noreply.github.com>
2025-11-10 12:35:34 +0530

    replaced dogBio.js with script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const myName = "Dev Rockey";
+const luckyNumber = 1;
+
+let nextYearLuckyNumber = luckyNumber+10;
+let remaninder = nextYearLuckyNumber % 5;
+console.log("Hello, my name is "+myName+" .");
+console.log(`My current lucky number is ${luckyNumber}.`);
+console.log(`My current lucky number + 10 is ${nextYearLuckyNumber}.`);
+console.log("The remainder when dividing that by 5 is " + remaninder+" .");
+
+
+
